3d9cee125ff58eba8d8e46b11b93ded5386b4907
Nikolajus Karpovas <superheromatch@gmail.com>
2019-11-03 18:14:39 +0100

    Add update profile call to server

--- SuperheroMatch/Controller/Main/Profile/EditProfileInfoVC.swift
+++ SuperheroMatch/Controller/Main/Profile/EditProfileInfoVC.swift
@@ -13,6 +13,8 @@ class EditProfileInfoVC: UIViewController, UITextViewDelegate {
     let userDB = UserDB.sharedDB
     var user: User?
     
+    var update: Update?
+    
     var kmMiSV: UIStackView!
     
     let superPowerLabel: UILabel = {
@@ -94,6 +96,8 @@ class EditProfileInfoVC: UIViewController, UITextViewDelegate {
             
             configureDistanceUnitButtons()
         }
+        
+        update = Update()
     }
     
     func configureDistanceUnitButtons() {
@@ -213,5 +217,75 @@ class EditProfileInfoVC: UIViewController, UITextViewDelegate {
         }
         
     }
+    
+    func configureUpdateProfileRequest() -> [String: Any]? {
+        
+        let (dbErr, user) = self.userDB.getUser()
+        if case .SQLError = dbErr {
+            print("###########  getUser dbErr  ##############")
+            print(dbErr)
+        }
+        
+        if user != nil {
+            var params = [String: Any]()
+            
+            params["id"] = user!.userID
+            params["email"] = user!.email
+            params["name"] = user!.name
+            params["superheroName"] = user!.superheroName
+            params["mainProfilePicUrl"] = user!.mainProfilePicUrl
+            params["gender"] = user!.gender
+            params["lookingForGender"] = user!.lookingForGender
+            params["age"] = user!.age
+            params["lookingForAgeMin"] = user!.lookingForAgeMin
+            params["lookingForAgeMax"] = user!.lookingForAgeMax
+            params["lookingForDistanceMax"] = user!.lookingForDistanceMax
+            params["distanceUnit"] = user!.distanceUnit
+            params["lat"] = user!.lat
+            params["lon"] = user!.lon
+            params["birthday"] = user!.birthday
+            params["country"] = user!.country
+            params["city"] = user!.city
+            params["superPower"] = user!.superPower
+            params["accountType"] = user!.accountType
+            
+             return params
+        }
+        
+       
+        return nil
+    }
+    
+    func update(update: Update, params: [String: Any]) {
+        
+        update.update(params: params) { json, error in
+            do {
+                
+                //Convert to Data
+                let jsonData = try JSONSerialization.data(withJSONObject: json!, options: JSONSerialization.WritingOptions.prettyPrinted)
+                
+                //In production, you usually want to try and cast as the root data structure. Here we are casting as a dictionary. If the root object is an array cast as [Any].
+                let updateResponse = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.mutableContainers) as? AnyObject
+                
+                let response = try UpdateProfileResponse(json: updateResponse as! [String : Any])
+
+                if !response.updated {
+                    print("Something went wrong!")
+                } else {
+                    print("All good!")
+                }
+                
+            } catch {
+                print("update catch")
+            }
+        }
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        update(update: self.update!, params: self.configureUpdateProfileRequest()!)
+    }
+    
+    // override func view
     
 }

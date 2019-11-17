fa5a1bc1551da3c1cdc28274c3c0476a0de1d787
Nikolajus Karpovas <superheromatch@gmail.com>
2019-11-17 20:49:58 +0100

    Update Suggestions UI

--- SuperheroMatch/Controller/Main/Suggestions/UserSuggestionsVC.swift
+++ SuperheroMatch/Controller/Main/Suggestions/UserSuggestionsVC.swift
@@ -25,46 +25,46 @@ class UserSuggestionsVC: UIViewController, CLLocationManagerDelegate {
     var longitude: Double?
     
     let profileImageView: UIImageView = {
-        let piv = UIImageView(image: UIImage(named: "test"))
+        let piv = UIImageView(image: UIImage(named: "test10"))
         piv.contentMode = .scaleAspectFill
+        piv.layer.cornerRadius = 10
         piv.clipsToBounds = true
         piv.tappable = true
+        piv.layer.shadowColor = UIColor.black.cgColor
+        piv.layer.shadowOpacity = 1
+        piv.layer.shadowOffset = CGSize.zero
+        piv.layer.shadowRadius = 10
+        piv.layer.shadowPath = UIBezierPath(roundedRect: piv.bounds, cornerRadius: 10).cgPath
         
         return piv
     }()
     
-    let userName: UILabel = {
+    let userNameAge: UILabel = {
         let userName = UILabel()
         userName.font = UIFont(name: "Gotham Book", size: 22)
         userName.textAlignment = .center
+        userName.textColor = .white
         
         return userName
     }()
     
-    let genderAge: UILabel = {
+    let city: UILabel = {
         let lbl = UILabel()
-        lbl.font = UIFont(name: "Gotham Book", size: 16)
+        lbl.font = UIFont(name: "Gotham Book", size: 18)
         lbl.textAlignment = .center
+        lbl.textColor = .white
         
         return lbl
     }()
     
-    let superPowerImageView: UIImageView = {
-        let piv = UIImageView(image: UIImage(named: "superpower"))
-        piv.contentMode = .scaleAspectFill
-        piv.clipsToBounds = true
-        
-        return piv
-    }()
-    
     let superPower: UILabel = {
         let lbl = UILabel()
-        lbl.font = UIFont(name: "Gotham Book", size: 18)
-        lbl.textAlignment = .left
-        lbl.text = "Awesome Super Power that I have and it is just test to see how it looks like on the screen when character length is maxed out."
+        lbl.font = UIFont(name: "Gotham Book", size: 16)
+        lbl.textAlignment = .center
         lbl.numberOfLines = 0
         lbl.lineBreakMode = .byWordWrapping
         lbl.sizeToFit()
+        lbl.textColor = .white
         
         return lbl
     }()
@@ -77,12 +77,68 @@ class UserSuggestionsVC: UIViewController, CLLocationManagerDelegate {
         return btn
     }()
     
+    lazy var superPowerButton: UIButton = {
+        var btn = UIButton(type: .system)
+        btn.setImage(UIImage(named: "superpower")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        btn.addTarget(self, action: #selector(superpowerTapped), for: .touchUpInside)
+        
+        return btn
+    }()
+    
     lazy var likeButton: UIButton = {
         var btn = UIButton(type: .system)
         btn.setImage(UIImage(named: "like")?.withRenderingMode(.alwaysOriginal), for: .normal)
         btn.addTarget(self, action: #selector(likeTapped), for: .touchUpInside)
         
         return btn
+    }()
+    
+    let gradientView: UIView = {
+        let view = UIView()
+        
+        return view
+    }()
+    
+    let suggestionDetailsView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        return view
+    }()
+    
+    lazy var closeSuggestionDetailsViewButton: UIButton = {
+        var btn = UIButton(type: .system)
+        btn.setImage(UIImage(named: "close")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        btn.addTarget(self, action: #selector(closeSuggestionDetailsViewTapped), for: .touchUpInside)
+        
+        return btn
+    }()
+    
+    let suggestionNameAge: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont(name: "Gotham Book", size: 22)
+        lbl.textAlignment = .left
+        
+        return lbl
+    }()
+    
+    let suggestionCity: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont(name: "Gotham Book", size: 18)
+        lbl.textAlignment = .left
+        
+        return lbl
+    }()
+    
+    let suggestionSuperPower: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont(name: "Gotham Book", size: 16)
+        lbl.textAlignment = .left
+        lbl.numberOfLines = 0
+        lbl.lineBreakMode = .byWordWrapping
+        lbl.sizeToFit()
+        
+        return lbl
     }()
     
     override func viewDidLoad() {
@@ -129,31 +185,103 @@ class UserSuggestionsVC: UIViewController, CLLocationManagerDelegate {
     func configureUI() {
         
         view.addSubview(profileImageView)
-        profileImageView.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: view.frame.width, height: view.frame.height * 0.5)
+        profileImageView.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 5, paddingLeft: 5, paddingBottom: 0, paddingRight: 5, width: view.frame.width-10, height: view.frame.height * 0.70)
+        profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         profileImageView.callback = handleMainProfileImageTapped
         
-        view.addSubview(userName)
-        userName.anchor(top: profileImageView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 5, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 30)
+        view.addSubview(gradientView)
+        gradientView.anchor(top: nil, left: nil, bottom: profileImageView.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 10, paddingBottom: 5, paddingRight: 10, width: view.frame.width-10, height: 115)
+        gradientView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        view.addSubview(genderAge)
-        genderAge.anchor(top: userName.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 5, paddingRight: 0, width: 0, height: 25)
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.1).cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.locations = [0, 1]
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width-10, height: 115)
         
-        userName.text = "Superhero"
-        genderAge.text = "Gender, Age"
-        
-        view.addSubview(superPowerImageView)
-        superPowerImageView.anchor(top: genderAge.bottomAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 25, paddingLeft: 5, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
+        gradientView.layer.insertSublayer(gradientLayer, at: 0)
         
         view.addSubview(superPower)
-        superPower.anchor(top: genderAge.bottomAnchor, left: superPowerImageView.rightAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 5, paddingLeft: 10, paddingBottom: 0, paddingRight: 10, width: view.frame.width, height: 80)
+        superPower.anchor(top: nil, left: view.leftAnchor, bottom: profileImageView.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 10, paddingBottom: 0, paddingRight: 10, width: view.frame.width, height: 60)
+        superPower.text = "Awesome Super Power that I have and it is just test to see how it looks like on the screen when character length is maxed out."
+        
+        view.addSubview(city)
+        city.anchor(top: nil, left: view.leftAnchor, bottom: superPower.topAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 5, paddingRight: 0, width: 0, height: 25)
+        city.text = "Somewhere"
+        
+        view.addSubview(userNameAge)
+        userNameAge.anchor(top: nil, left: view.leftAnchor, bottom: city.topAnchor, right: view.rightAnchor, paddingTop: 5, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 30)
+        userNameAge.text = "Superhero, 34"
         
         view.addSubview(dislikeButton)
-        dislikeButton.anchor(top: superPower.bottomAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 10, paddingLeft: 20, paddingBottom: 0, paddingRight: 40, width: 40, height: 40)
+        dislikeButton.anchor(top: profileImageView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 10, paddingLeft: 20, paddingBottom: 0, paddingRight: 40, width: 50, height: 50)
+        
+        view.addSubview(superPowerButton)
+        superPowerButton.anchor(top: profileImageView.bottomAnchor, left: nil, bottom: nil, right: nil, paddingTop: 10, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
+        superPowerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         view.addSubview(likeButton)
-        likeButton.anchor(top: superPower.bottomAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 10, paddingLeft: 40, paddingBottom: 0, paddingRight: 20, width: 40, height: 40)
+        likeButton.anchor(top: profileImageView.bottomAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 10, paddingLeft: 40, paddingBottom: 0, paddingRight: 20, width: 50, height: 50)
+        
+        configureSuggestionDetailsView()
         
     }
+    
+    func configureSuggestionDetailsView() {
+        
+        view.addSubview(suggestionDetailsView)
+        suggestionDetailsView.anchor(top: nil, left: nil, bottom: view.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 5, paddingBottom: 0, paddingRight: 5, width: view.frame.width-10, height: view.frame.height * 0.40)
+        suggestionDetailsView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        suggestionDetailsView.addSubview(closeSuggestionDetailsViewButton)
+        closeSuggestionDetailsViewButton.anchor(top: suggestionDetailsView.topAnchor, left: nil, bottom: nil, right: suggestionDetailsView.rightAnchor, paddingTop: 5, paddingLeft: 0, paddingBottom: 0, paddingRight: 5, width: 40, height: 40)
+        
+        suggestionDetailsView.addSubview(suggestionNameAge)
+        suggestionNameAge.anchor(top: closeSuggestionDetailsViewButton.bottomAnchor, left: suggestionDetailsView.leftAnchor, bottom: nil, right: suggestionDetailsView.rightAnchor, paddingTop: 5, paddingLeft: 10, paddingBottom: 0, paddingRight: 10, width: 0, height: 30)
+        suggestionNameAge.text = "Superhero, 34"
+        
+        suggestionDetailsView.addSubview(suggestionCity)
+        suggestionCity.anchor(top: suggestionNameAge.bottomAnchor, left: suggestionDetailsView.leftAnchor, bottom: nil, right: suggestionDetailsView.rightAnchor, paddingTop: 0, paddingLeft: 10, paddingBottom: 5, paddingRight: 10, width: 0, height: 25)
+        suggestionCity.text = "Somewhere"
+        
+        suggestionDetailsView.addSubview(suggestionSuperPower)
+        suggestionSuperPower.anchor(top: suggestionCity.bottomAnchor, left: suggestionDetailsView.leftAnchor, bottom: nil, right: suggestionDetailsView.rightAnchor, paddingTop: 0, paddingLeft: 10, paddingBottom: 0, paddingRight: 10, width: suggestionDetailsView.frame.width, height: 60)
+        suggestionSuperPower.text = "Awesome Super Power that I have and it is just test to see how it looks like on the screen when character length is maxed out."
+        
+        self.suggestionDetailsView.isHidden = true
+    
+    }
+    
+    func viewSlideInFromTopToBottom(view: UIView) {
+        
+        let transition: CATransition = CATransition()
+        transition.duration = 0.5
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        transition.type = .push
+        transition.subtype = CATransitionSubtype.fromBottom
+        view.layer.add(transition, forKey: kCATransition)
+        
+    }
+    
+    func viewSlideInFromBottomToTop(view: UIView) {
+        
+        let transition: CATransition = CATransition()
+        transition.duration = 0.5
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        transition.type = .push
+        transition.subtype = CATransitionSubtype.fromTop
+        view.layer.add(transition, forKey: kCATransition)
+        
+    }
+    
+    @objc func closeSuggestionDetailsViewTapped() {
+        
+        viewSlideInFromTopToBottom(view: self.suggestionDetailsView)
+        self.suggestionDetailsView.isHidden = true
+        
+    }
+    
     
     @objc func handleMainProfileImageTapped() {
         print("handleMainProfileImageTapped")
@@ -164,12 +292,19 @@ class UserSuggestionsVC: UIViewController, CLLocationManagerDelegate {
         
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
             
-            self.profileImageView.image = UIImage(named: "barzomer")
-            self.userName.text = "Bar Zomer"
-            self.genderAge.text = "Female, 21"
+            self.profileImageView.image = UIImage(named: "test1")
+            self.userNameAge.text = "Bar Zomer, 26"
+            self.city.text = "Somewhere"
             self.superPower.text = "Amazing Eyes"
             
         }, completion: nil)
+        
+    }
+    
+    @objc func superpowerTapped() {
+        
+        self.suggestionDetailsView.isHidden = false
+        viewSlideInFromBottomToTop(view: self.suggestionDetailsView)
         
     }
     
@@ -178,9 +313,9 @@ class UserSuggestionsVC: UIViewController, CLLocationManagerDelegate {
         
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
             
-            self.profileImageView.image = UIImage(named: "missbuscemi")
-            self.userName.text = "Isabella Buscemi"
-            self.genderAge.text = "Female, 21"
+            self.profileImageView.image = UIImage(named: "test3")
+            self.userNameAge.text = "Isabella Buscemi, 26"
+            self.city.text = "Somewhere"
             self.superPower.text = "Bootylicious"
             
         }, completion: nil)
